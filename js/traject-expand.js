d8cb900805e65ee33e239b59ab1ddc8f7e404b58
jimvanzummeren <j.van.zummeren@gmail.com>
2013-12-17 18:36:22 +0100

    anim

--- js/traject-expand.js
+++ js/traject-expand.js
@@ -1,2 +1,22 @@
 /*traject-short*/
 /*transport*/
+
+$(function(){
+
+	$('.traject-short').click(function(){
+		var $li = $(this).closest('li');
+
+		if($li.hasClass('is-selected')){
+			$li.removeClass('is-selected');
+			$li.find('.transport').slideUp();
+		}else{
+			$li.addClass('is-selected');
+			//$li.find('.transport').css('position', 'relative');
+			//$li.find('.transport').css('margin-top', '-300px');
+			$li.find('.transport').slideDown();
+			/*$li.find('.transport').animate({
+				marginTop : 0
+			}, 500);	*/
+		}
+	})
+})
